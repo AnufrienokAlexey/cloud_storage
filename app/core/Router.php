9e321e7dbd034258011a7cc7b9ff4f3285d4e85d
Aleksey Anufrienok <alex.anufrienok@gmail.com>
2023-07-13 07:20:01 +0300

    fix#1

---
+++ app/core/Router.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\core;
+
+class Router
+{
+    public function list()
+    {
+        echo 'list()';
+    }
+
+    public function add()
+    {
+        echo 'add()';
+    }
+}
--- index.php
+++ index.php
@@ -1,8 +1,37 @@
 <?php
+
+use app\core\Router;
+
+require 'app\lib\dev.php';
+
+spl_autoload_register(function ($class)
+{
+    $path = str_replace('\\', '/', $class.'.php');
+    if (file_exists($path)) {
+        require $path;
+    }
+});
+
 $urlList = [
     'funds' => [
         'GET' => 'Funds::list()',
         'POST' => 'Funds::add()'
     ]
 ];
-var_dump($_SERVER['REQUEST_URI']); // /test
+
+$url = trim($_SERVER['REQUEST_URI'], '/');
+
+if (array_key_exists($url, $urlList)) {
+    foreach ($urlList as $item) {
+        foreach (array_keys($item) as $method) {
+            if ($method == $_SERVER['REQUEST_METHOD']) {
+                echo $method;
+                $obj = new Router();
+                $obj->list();
+                return;
+            }
+        }
+    }
+} else {
+    echo 'Ошибка 404';
+}
