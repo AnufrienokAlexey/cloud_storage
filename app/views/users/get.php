0a5cc0d9c2b4cea48aaf2da808b784a4ffbc1d11
Aleksey Anufrienok <alex.anufrienok@gmail.com>
2023-07-15 02:33:24 +0300

    fix8

--- app/views/users/get.php
+++ app/views/users/get.php
@@ -1,8 +1,20 @@
 <h4>Получить пользователя по id</h4>
-<ol>
+<form class="row g-3" action="get" method="get">
+    <div class="col-auto">
+        <label for="staticEmail2" class="visually-hidden">Email</label>
+        <input type="text" readonly class="form-control-plaintext" id="staticEmail2" value="Введите id пользователя">
+    </div>
+    <div class="col-auto">
+        <input type="text" class="form-control" id="inputPassword2" placeholder="id" name="id" required>
+    </div>
+    <div class="col-auto">
+        <button type="submit" class="btn btn-primary mb-3">Найти пользователя</button>
+    </div>
+</form>
+<ul>
     <?php foreach ($vars as $item) {?>
-        <li><?=$item['email']; ?></li>
+        <li><?='id = '.$item['id'].', email = '.$item['email']; ?></li>
     <?php }?>
-</ol>
+</ul>
 </body>
 </html>
