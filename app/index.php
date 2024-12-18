afcdd91423bd86bb2941d8d4459e0c7957391db9
Aleksey Anufrienok <alex.anufrienok@gmail.com>
2024-12-18 05:33:48 +0300

    api

--- app/index.php
+++ app/index.php
@@ -1,10 +1,37 @@
 <?php
 
-require __DIR__ . '\..\vendor\autoload.php';
+require_once __DIR__ . '/config/config.php';
+require_once ROOT . '/../vendor/autoload.php';
+echo 'index.php';
 
 $urlList = [
-    'main' => 'action',
+    '/users' => [
+        ['GET', 'list'],
+        ['GET', 'get'],
+        ['PUT', 'update'],
+    ],
+    '/admin/users' => [
+        ['GET', 'list'],
+        ['GET', 'get'],
+        ['DELETE', 'delete'],
+        ['PUT', 'update'],
+    ],
+    '/files' => [
+        ['GET', 'list'],
+        ['GET', 'get'],
+        ['POST', 'add'],
+        ['PUT', 'rename'],
+        ['DELETE', 'remove'],
+        ['GET', 'share'],
+        ['PUT', 'share'],
+        ['DELETE', 'share'],
+    ],
+    '/directories' => [
+        ['POST', 'add'],
+        ['PUT', 'rename'],
+        ['GET', 'get'],
+        ['DELETE', 'delete'],
+    ],
 ];
 
 return $urlList;
-
