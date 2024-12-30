844252a699b14740cd61015e6e05dd76a9aa2d83
Aleksey Anufrienok <alex.anufrienok@gmail.com>
2024-12-30 09:44:45 +0300

    fix

--- app/core/Connect.php
+++ app/core/Connect.php
@@ -8,69 +8,75 @@ class Connect extends Db
 {
     //protected static string $dbName = 'cloud_storage';
 
-    public static function db(): object
+    public static function db(): PDO
     {
-        return new Db(
+        $db = new Db(
             CONFIG['host'],
             CONFIG['dbname'],
             CONFIG['charset'],
             CONFIG['username'],
             CONFIG['password'],
         );
-    }
-
-    public static function getAllDatabases(): false|array
-    {
-        $host = self::db()->host;
-        $username = self::db()->username;
-        $password = self::db()->password;
+        $dbVars = get_object_vars($db);
 
         try {
-            $pdo = new PDO(
-                "mysql:host=$host",
-                "$username",
-                "$password"
+            return new PDO(
+                "mysql:host=$dbVars[host]",
+                "$dbVars[username]",
+                "$dbVars[password]"
             );
-            return $pdo->query('SHOW DATABASES')->fetchAll(PDO::FETCH_COLUMN);
-        } catch (\PDOException $e) {
+        } catch (\Exception $e) {
             error_log($e->getMessage());
             die('MySQL is not connected!');
         }
     }
 
+    public static function getAllDatabases(): false|array
+    {
+        return self::db()
+            ->query('SHOW DATABASES')
+            ->fetchAll(PDO::FETCH_COLUMN);
+    }
+
     public static function createNewDb($dbname): void
     {
-        $host = self::db()->host;
-        $username = self::db()->username;
-        $password = self::db()->password;
-
+//        $host = self::db()->host;
+//        $username = self::db()->username;
+//        $password = self::db()->password;
+//
+//        try {
+//            $pdo = new PDO(
+//                "mysql:host=$host",
+//                "$username",
+//                "$password"
+//            );
+//            $stm = $pdo->prepare(
+//                "CREATE DATABASE $dbname COLLATE utf8_general_ci"
+//            );
+//            $stm->execute();
+//        } catch (\PDOException $e) {
+//            error_log($e->getMessage());
+//        }
         try {
-            $pdo = new PDO(
-                "mysql:host=$host",
-                "$username",
-                "$password"
-            );
-            $stm = $pdo->prepare(
+            self::db()->prepare(
                 "CREATE DATABASE $dbname COLLATE utf8_general_ci"
             );
-            $stm->execute();
         } catch (\PDOException $e) {
             error_log($e->getMessage());
         }
     }
 
-    public static function connect(): PDO
+    public static function connect(): void
     {
         $databases = self::getAllDatabases();
 
-        if (!in_array(self::db()->dbname, $databases)) {
-            self::createNewDb(self::db()->dbname);
+        if (!in_array(CONFIG['dbname'], $databases)) {
+            self::createNewDb(CONFIG['dbname']);
         }
 
         $pdo = self::db();
         $stm = $pdo->prepare("SELECT * FROM `cloud_storage`");
-        $res = $stm->execute;
-
-        return $res;
+        dump($stm);
+        $stm->execute();
     }
 }
