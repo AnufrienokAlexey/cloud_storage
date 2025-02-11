61e979cc7d5022e5810f372c4a8785fdf6a78a0b
Alexey Anufrienok <alex.anufrienok@gmail.com>
2025-02-11 06:39:29 +0300

    fix db class

--- app/Core/Db.php
+++ app/Core/Db.php
@@ -8,16 +8,21 @@ use PDOException;
 class Db
 {
     protected static ?Db $_instance = null;
+    private PDO $pdo;
 
     public function __construct($host, $dbname, $charset, $username, $password)
     {
         $dsn = "mysql:host=$host;dbname=$dbname;charset=$charset";
+        $options = [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_EMULATE_PREPARES => false,
+        ];
         try {
-            return new PDO($dsn, $username, $password);
+            $this->pdo = new PDO($dsn, $username, $password, $options);
         } catch (PDOException $e) {
             echo $e->getMessage();
         }
-        return false;
     }
 
     private function __clone()
@@ -28,7 +33,7 @@ class Db
     {
     }
 
-    public static function getInstance(): self
+    public static function getInstance(): PDO
     {
         if (is_null(self::$_instance)) {
             self::$_instance = new self(
@@ -39,7 +44,7 @@ class Db
                 CONFIG['password'],
             );
         }
-        return self::$_instance;
+        return self::$_instance->pdo;
     }
 
     public function getConnection()
