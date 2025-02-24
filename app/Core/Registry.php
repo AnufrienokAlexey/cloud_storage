0d62e18c2d7cbb8e1e193d13d91700c8baec079f
Alexey Anufrienok <alex.anufrienok@gmail.com>
2025-02-24 07:24:05 +0300

    added registry class

---
+++ app/Core/Registry.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace app\Core;
+
+use InvalidArgumentException;
+
+
+class Registry
+{
+    protected static ?Registry $instances = null;
+    protected static array $registry = [];
+
+    public function __construct()
+    {
+    }
+
+    public static function getInstance(): ?Registry
+    {
+        if (is_null(self::$instances)) {
+            self::$instances = new Registry();
+        }
+        return self::$instances;
+    }
+
+    final public function set(string $key, $value): void
+    {
+        if (!in_array($key, self::$registry)) {
+            throw new InvalidArgumentException("Указан неверный ключ - $key.");
+        }
+        self::$registry[$key] = $value;
+    }
+
+    final public function get(string $key)
+    {
+        if (!in_array($key, self::$registry)) {
+            throw new InvalidArgumentException("Указан неверный ключ - $key.");
+        }
+        return self::$registry[$key] ?? null;
+    }
+
+}
