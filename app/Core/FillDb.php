9faa34a05d08d1cb6ac084d0ae20b7c37e094686
Alexey Anufrienok <alex.anufrienok@gmail.com>
2025-03-06 12:23:35 +0300

    db auto added data

--- app/Core/FillDb.php
+++ app/Core/FillDb.php
@@ -2,18 +2,16 @@
 
 namespace app\Core;
 
+use Faker\Factory;
+
 class FillDb
 {
     private static string $usernameUrl = 'https://potterapi-fedeperin.vercel.app/en/characters';
-    private static string $randomDataUrl = 'https://random-data-api.com/api/v3/projects/d7da5248-1a22-45cc-b051-58b5bdc04385';
-    private static string $randomApiKey = 'C9cpzvOi937DrLEke0y85Q';
 
     private static function getResponse($url): bool|string
     {
-        $headers = ['X-API-Key: Tvv_gsSIolV9UHg_EvRlYg'];
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         $response = curl_exec($ch);
         curl_close($ch);
@@ -28,19 +26,17 @@ class FillDb
             $users = json_decode(self::getResponse(self::$usernameUrl), true);
             foreach ($users as $user) {
                 $username = $user['fullName'];
-                $randomData = json_decode(self::getResponse(self::$randomDataUrl), true);
-                $email = $randomData['email'];
-                $password = $randomData['password'];
-                $speciality = $randomData['speciality'];
-                $techSkill = $randomData['tech_skill'];
+                $faker = Factory::create();
+                $email = $faker->email();
+                $password = hash('sha256', $faker->password());
+                $birthdate = $user['birthdate'];
                 $role = $arr[array_rand($arr, 1)];
 
                 $response[] = [
                     'username' => $username,
                     'email' => $email,
                     'password' => $password,
-                    'speciality' => $speciality,
-                    'tech_skill' => $techSkill,
+                    'birthdate' => $birthdate,
                     'role' => $role
                 ];
             }
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -7,6 +7,7 @@ namespace Composer\Autoload;
 class ComposerStaticInitd751713988987e9331980363e24189ce
 {
     public static $files = array (
+        '6e3fae29631ef280660b3cdad06f25a8' => __DIR__ . '/..' . '/symfony/deprecation-contracts/function.php',
         '0e6d7bf4a5811bfa5cf40c5ccd6fae6a' => __DIR__ . '/..' . '/symfony/polyfill-mbstring/bootstrap.php',
         '667aeda72477189d0494fecd327c3641' => __DIR__ . '/..' . '/symfony/var-dumper/Resources/functions/dump.php',
     );
@@ -20,6 +21,14 @@ class ComposerStaticInitd751713988987e9331980363e24189ce
         array (
             'Symfony\\Polyfill\\Mbstring\\' => 26,
             'Symfony\\Component\\VarDumper\\' => 28,
+        ),
+        'P' => 
+        array (
+            'Psr\\Container\\' => 14,
+        ),
+        'F' => 
+        array (
+            'Faker\\' => 6,
         ),
     );
 
@@ -35,6 +44,14 @@ class ComposerStaticInitd751713988987e9331980363e24189ce
         'Symfony\\Component\\VarDumper\\' => 
         array (
             0 => __DIR__ . '/..' . '/symfony/var-dumper',
+        ),
+        'Psr\\Container\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/psr/container/src',
+        ),
+        'Faker\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/fakerphp/faker/src/Faker',
         ),
     );
 
